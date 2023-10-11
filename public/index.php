45a2f5149f6ceec94a00766f3749ae065913ecfb
Sigsign <sig@signote.cc>
2023-10-12 00:17:26 +0900

    [WIP] Add a front controller

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Front controller
+ *
+ * @copyright 2023 Sigsign
+ * @license Apache-2.0 or MIT-0
+ * @author Sigsign <sig@signote.cc>
+ */
+
+declare(strict_types=1);
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
+use Symfony\Component\Routing\Route;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+$routes = new Routing\RouteCollection();
+$routes->add('bbs_cgi', new Route('/test/bbs.cgi', [
+    '_controller' => function () {
+        return new Response('It works!');
+    }
+]));
+
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('Unexpected Error', 500);
+}
+
+$response->prepare($request);
+$response->send();
